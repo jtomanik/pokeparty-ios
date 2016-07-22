9667828dd3e916df44d6dec9739dce971c881c23
Jaroslaw Gliwinski <jaroslaw.m.gliwinski@gmail.com>
2016-07-22 16:48:28 +0200

    Add google login sdk

--- Podfile
+++ Podfile
@@ -2,4 +2,5 @@ use_frameworks!
 
 target "PokeParty" do
 	pod 'Moya'
+	pod 'Google/SignIn'
 end
--- PokeParty/AppDelegate.swift
+++ PokeParty/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
     var window: UIWindow?
 
@@ -20,11 +20,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         setupWindow()
+        setupGoogleLogin()
         return true
     }
 
-    private func setupWindow() {
+    private func setupGoogleLogin() {
+        var configureError: NSError?
+        GGLContext.sharedInstance().configureWithError(&configureError)
+        assert(configureError == nil, "Error configuring Google services: \(configureError)")
 
+        GIDSignIn.sharedInstance().delegate = self
+    }
+
+    func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!, withError error: NSError!) {
+        if (error == nil) {
+            // Perform any operations on signed in user here.
+            let userId = user.userID                  // For client-side use only! (haha)
+        } else {
+            print("\(error.localizedDescription)")
+        }
+    }
+
+    func application(application: UIApplication, openURL url: NSURL, options: [String: AnyObject]) -> Bool {
+        return GIDSignIn.sharedInstance().handleURL(
+            url,
+            sourceApplication: options[UIApplicationOpenURLOptionsSourceApplicationKey] as? String,
+            annotation: options[UIApplicationOpenURLOptionsAnnotationKey]
+        )
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    private func setupWindow() {
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.backgroundColor = UIColor.whiteColor()
         window?.rootViewController = mainCoordinator.rootViewController

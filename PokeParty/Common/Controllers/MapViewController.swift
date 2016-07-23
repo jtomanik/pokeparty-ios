76a1ef2e6c1fa3a271f7a1abaf2773e0297ae4f9
Karol Kubicki <karol777@gmail.com>
2016-07-23 13:26:12 +0200

    Create event stuff

---
+++ PokeParty/Common/Controllers/MapViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MapViewController.swift
+//  PokeParty
+//
+//  Created by Karol Kubicki on 23.07.2016.
+//  Copyright © 2016 tooploox. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    private var currentAnnotation: MKPointAnnotation? = nil
+    
+    lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        map.showsUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+        let tap = UITapGestureRecognizer(target: self, action: #selector(mapTap))
+        map.addGestureRecognizer(tap)
+        return map
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(mapView)
+        mapView.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor).active = true
+        mapView.bottomAnchor.constraintEqualToAnchor(bottomLayoutGuide.topAnchor).active = true
+        mapView.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor).active = true
+        mapView.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor).active = true
+        
+        CLLocationManager().requestAlwaysAuthorization()
+    }
+
+    @objc func mapTap(recognizer: UIGestureRecognizer) {
+        if currentAnnotation != nil {
+            mapView.removeAnnotation(currentAnnotation!)
+        }
+        let point = recognizer.locationInView(mapView)
+        let coordinate = mapView.convertPoint(point, toCoordinateFromView: mapView)
+        
+        currentAnnotation = MKPointAnnotation()
+        currentAnnotation!.coordinate = coordinate
+        mapView.addAnnotation(currentAnnotation!)
+    }
+
+}

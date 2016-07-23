074cc9558ca402bf406db95fbc7bd1ac07d63c10
Jakub Warchoł <jakub@tooploox.com>
2016-07-23 13:02:30 +0200

    Redesign sign on

--- PokeParty/Domain/GoogleSignOnViewController.swift
+++ PokeParty/Domain/GoogleSignOnViewController.swift
@@ -17,8 +17,9 @@ class GoogleSignOnViewController: UIViewController, GIDSignInUIDelegate {
     private lazy var googleSignOnButton: UIButton = {
         let button = UIButton()
         button.setTitle("Google sign on", forState: .Normal)
-        button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        button.layer.cornerRadius = 3.0
+        button.setTitleColor(UIColor.appButtonBorderYellowColor(), forState: .Normal)
+        button.titleLabel?.font = UIFont.systemFontOfSize(20.0, weight: UIFontWeightMedium)
+        button.layer.cornerRadius = 9.0
         button.layer.borderWidth = 3.0
         button.backgroundColor = UIColor.appButtonBackgroundBlueColor()
         button.layer.borderColor = UIColor.appButtonBorderYellowColor().CGColor
@@ -26,10 +27,33 @@ class GoogleSignOnViewController: UIViewController, GIDSignInUIDelegate {
         return button
     }()
 
+    private let logo: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "pokeParty")
+        imageView.contentMode = .ScaleAspectFill
+        
+        return imageView
+    }()
+
     weak var delegate: GoogleSignOnViewControllerDelegate?
 
     override func loadView() {
         view = UIView()
+
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+
+        view.addSubview(logo)
+        view.addSubview(googleSignOnButton)
+
+        logo.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activateConstraints([
+            logo.topAnchor.constraintEqualToAnchor(view.topAnchor, constant: 120.0),
+            logo.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor),
+            logo.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor),
+        ])
+
         setupGoogleSignOnButton()
         view.backgroundColor = UIColor.appSplashScreenBackgroundColor()
     }
@@ -41,11 +65,10 @@ class GoogleSignOnViewController: UIViewController, GIDSignInUIDelegate {
     }
 
     private func setupGoogleSignOnButton() {
-        view.addSubview(googleSignOnButton)
         googleSignOnButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activateConstraints([
-            googleSignOnButton.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor, constant: 50.0),
-            googleSignOnButton.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor, constant: -50.0),
+            googleSignOnButton.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor, constant: 30.0),
+            googleSignOnButton.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor, constant: -30.0),
             googleSignOnButton.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor, constant: -50.0),
             googleSignOnButton.heightAnchor.constraintEqualToConstant(60.0),
         ])

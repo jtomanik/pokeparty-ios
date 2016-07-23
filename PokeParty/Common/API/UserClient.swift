ca3e35ff615106d502c82933e52f4ee7f5bb80ab
Jaroslaw Gliwinski <jaroslaw.m.gliwinski@gmail.com>
2016-07-23 13:37:08 +0200

    Event, Party clients, build fixes

--- PokeParty/Common/API/UserClient.swift
+++ PokeParty/Common/API/UserClient.swift
@@ -21,7 +21,7 @@ class UserClient {
 
     func googleAuth(id: String, completion: UserClientCompletion) {
         let action = SignupAction.googleAuth
-        let request = requestFor(action, queryItems: [NSURLQueryItem(name: "id", value: id)])
+        let request = apiClient.requestFor(action, queryItems: [NSURLQueryItem(name: "id", value: id)])
 
         apiClient.send(request) { json, response, error in
             guard let json = json else {
@@ -35,7 +35,7 @@ class UserClient {
 
     func userSetup(user: User, completion: UserClientCompletion) {
         let action = SignupAction.userSetup
-        let request = try! requestFor(action, payload: UserAdapter.encode(model: user)?.rawData())
+        let request = try! apiClient.requestFor(action, payload: UserAdapter.encode(model: user)?.rawData())
 
         apiClient.send(request) { json, response, error in
             guard let json = json else {
@@ -45,40 +45,6 @@ class UserClient {
             let user = UserAdapter.decode(raw: json)
             completion(user, nil)
         }
-    }
-
-    func requestFor(action: Action, queryItems: [NSURLQueryItem]? = nil, payload: NSData? = nil) -> NSMutableURLRequest {
-        let url = NSURL(string: action.absoluteString)!
-        var request = NSMutableURLRequest(URL: url)
-
-        switch action.method {
-        case .get:
-            guard let queryItems = queryItems else {
-                assertionFailure("GET has to have Query Items (at least an empty array)")
-                return request
-            }
-            request.HTTPMethod = "GET"
-
-            let urlComponents = NSURLComponents(string: action.absoluteString)
-            urlComponents?.queryItems = queryItems
-            let url = urlComponents!.URL!
-
-            request = NSMutableURLRequest(URL: url)
-        case .post:
-            guard let payload = payload else {
-                assertionFailure("POST has to have Body")
-                return request
-            }
-            request.HTTPMethod = "POST"
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.setValue("application/json", forHTTPHeaderField: "Accept")
-
-            request.HTTPBody = payload
-        default:
-            assertionFailure("Unsupported Action Method")
-        }
-
-        return request
     }
 }
 
--- PokeParty/Domain/App/EventsViewController.swift
+++ PokeParty/Domain/App/EventsViewController.swift
@@ -29,9 +29,9 @@ class EventsViewController: UITableViewController {
         super.viewDidLoad()
 
         events = [
-            Event(name: "event", ownerId: "asd"),
-            Event(name: "event1", ownerId: "asd"),
-            Event(name: "event2", ownerId: "asd")
+            Event(name: "event", description: "Event description", latitude: 0, longitude: 0, ownerId: "asd"),
+            Event(name: "event1", description: "Event description", latitude: 0, longitude: 0, ownerId: "asd"),
+            Event(name: "event2", description: "Event description", latitude: 0, longitude: 0, ownerId: "asd")
         ]
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(onAddEvent))

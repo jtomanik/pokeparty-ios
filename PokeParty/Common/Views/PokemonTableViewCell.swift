f6ca0c9fd46663d936192a4a03b665d516c1b33a
Jakub Warchoł <jakub@tooploox.com>
2016-07-23 14:47:51 +0200

    List

--- PokeParty/Common/Views/PokemonTableViewCell.swift
+++ PokeParty/Common/Views/PokemonTableViewCell.swift
@@ -13,5 +13,29 @@ class PokemonTableViewCell: UITableViewCell {
     @IBOutlet weak var pokemonImage: UIImageView!
     @IBOutlet weak var pokemonNameLabel: UILabel!
     @IBOutlet weak var pokemonCombatPowerLabel: UILabel!
-    
+
+    func setupLayout() {
+
+        pokemonNameLabel.font = UIFont.systemFontOfSize(18.0, weight: UIFontWeightMedium)
+        pokemonNameLabel.textColor = UIColor.whiteColor()
+
+        pokemonCombatPowerLabel.font = UIFont.systemFontOfSize(12.0, weight: UIFontWeightMedium)
+        pokemonCombatPowerLabel.textColor = UIColor.appButtonBorderYellowColor()
+
+        pokemonImage.backgroundColor = UIColor.whiteColor()
+        pokemonImage.layer.cornerRadius = 30.0
+        pokemonImage.layer.borderWidth = 3.0
+        pokemonImage.layer.borderColor = UIColor.appButtonBorderYellowColor().CGColor
+        backgroundColor = UIColor.appSplashScreenBackgroundColor()
+    }
+
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//        setupLayout()
+//    }
+//
+//    override func prepareForReuse() {
+//        super.prepareForReuse()
+//        setupLayout()
+//    }
 }
